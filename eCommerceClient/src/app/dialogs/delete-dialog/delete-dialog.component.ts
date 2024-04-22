808cdf3d7f9e55d0c2fb952939ab85a521920230
HP <[ismayilia@code.edu.az]>
2024-04-22 10:49:32 +0400

    added global dialog class

--- eCommerceClient/src/app/dialogs/delete-dialog/delete-dialog.component.ts
+++ eCommerceClient/src/app/dialogs/delete-dialog/delete-dialog.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { BaseDialog } from '../base/base-dialog';
 
 @Component({
   selector: 'app-delete-dialog',
@@ -10,17 +11,15 @@ import { MatFormFieldModule } from '@angular/material/form-field';
   templateUrl: './delete-dialog.component.html',
   styleUrl: './delete-dialog.component.scss'
 })
-export class DeleteDialogComponent {
+export class DeleteDialogComponent extends BaseDialog<DeleteDialogComponent>{
   constructor(
 
-    public dialogRef: MatDialogRef<DeleteDialogComponent>,
+    dialogRef: MatDialogRef<DeleteDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: DeleteState,
-  ) { }
-
-
-  close(): void {
-    this.dialogRef.close();
+  ) { 
+    super(dialogRef);
   }
+
   
 }
 
---
+++ eCommerceClient/src/app/dialogs/file-upload-dialog/file-upload-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-file-upload-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './file-upload-dialog.component.html',
+  styleUrl: './file-upload-dialog.component.scss'
+})
+export class FileUploadDialogComponent {
+
+}
